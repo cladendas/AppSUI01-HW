4e1838328f7d41e6848bc2c9a3c6f38814640417
Vadim <rvdagi05@yandex.ru>
2021-12-12 21:01:13 +0300

    Initial Commit

---
+++ AppSUI01-HW/AppSUI01_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSUI01_HWApp.swift
+//  AppSUI01-HW
+//
+//  Created by cladendas on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppSUI01_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
